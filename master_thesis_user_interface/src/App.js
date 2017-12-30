9df61fa80ef86cf5caccdcbec41fb7a27b5fc29d
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2017-12-30 11:02:01 +0100

    react router added

--- master_thesis_user_interface/src/App.js
+++ master_thesis_user_interface/src/App.js
@@ -2,38 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Components/sidebar';
 import DataTypeSelection from './Components/js/DataTypeSelection';
+import Home from './Components/Home';
 import About from './Components/About';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state ={
-      selectedSidebarValue: 0
-    }
-  }
 
-  selectedValueFromSidebar(currentVal){
-    this.setState({
-      selectedSidebarValue: currentVal
-    },function(){
-    })
-  }
   render() {
-    const selectedVal = this.state.selectedSidebarValue;
-    let selectedComponent = '';
-    if(selectedVal ==1){
-      selectedComponent = <DataTypeSelection />
-    }else if(selectedVal ==2){
-      selectedComponent = <About />
-    }
+
     return (
-      <div className="row">
-        <div className="col-sm-3 col-md-6 col-lg-4" style={{'backgroundColor': 'lavender'}}>
-        <Sidebar sidebarValue={this.selectedValueFromSidebar.bind(this)}/>
-        </div>
-        <div className="col-sm-9 col-md-6 col-lg-8" style={{'backgroundColor': 'lavenderblush'}}>
-           {selectedComponent}
-        </div>
+      <div className="App">
+          <Router>
+            <div className="row">
+              <div className="col-sm-3 col-md-6 col-lg-4" style={{'backgroundColor': 'lavender'}}>
+                <Sidebar />
+              </div>
+              <div className="col-sm-9 col-md-6 col-lg-8" style={{'backgroundColor': 'lavenderblush'}}>
+                <Route exact path="/DataTypeSelection" component={DataTypeSelection} />
+                <Route path="/about" component={About} />
+              </div>
+            </div>
+          </Router>
       </div>
     );
   }
