2f9c2561a6c77a20da72ec825e13168895f5e5aa
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2017-12-30 16:22:34 +0100

    Drop Down box added in Data Set Selection

--- master_thesis_user_interface/src/Components/js/DataSetSelection.js
+++ master_thesis_user_interface/src/Components/js/DataSetSelection.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class DataSetSelection extends Component {
+  constructor(props){
+    super(props);
+    this.state = {value: "Finance"};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    console.log(this.state.value)
+    event.preventDefault();
+  }
   render() {
     return (
-      <div className="DataSetSelection">
-        My DataSetSelection
-      </div>
+      <form onSubmit={this.handleSubmit}>
+          <label>Category</label> <br />
+          <div>
+            <select value={this.state.value} onChange={this.handleChange}>
+               <option value="Finance">Finance</option>
+               <option value="Environment">Environment</option>
+               <option value="Technology">Technology</option>
+           </select>
+         </div>
+         <br />
+         <input type="submit" value="submit" />
+      </form>
     );
   }
 }
