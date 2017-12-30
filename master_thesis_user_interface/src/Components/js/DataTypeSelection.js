006cf1e9ace995771592e2ae5858adbfc7a8f0d3
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2017-12-30 13:33:38 +0100

    radio button for DataTypeSelection added

--- master_thesis_user_interface/src/Components/js/DataTypeSelection.js
+++ master_thesis_user_interface/src/Components/js/DataTypeSelection.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
 
 class DataTypeSelection extends Component {
-  handleSubmit(e){
-    console.log('fsdf');
+  constructor(props){
+    super(props);
+    this.state = {selectedOption: 'option1'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+// if I put console to this.state it will output as previous stored value
+  handleChange(event) {
+    this.setState({selectedOption: event.target.value});
+  }
+
+  handleSubmit(event){
+    console.log(this.state.selectedOption);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div className="radio">
           <label>
-            <input type="radio" name="optradio" value="option1" />
+            <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleChange}/>
             Option 1
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" name="optradio" value="option2" />
+            <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleChange}/>
             Option 2
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" name="optradio" value="option3" />
+            <input type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleChange}/>
             Option 3
           </label>
         </div>
+        <button className="btn btn-default" type="submit">submit</button>
+
       </form>
     );
   }
