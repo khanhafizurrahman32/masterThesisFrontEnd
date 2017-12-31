364924b5326c15cac882048bc7b7a035efb31310
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2017-12-31 11:36:45 +0100

    basic drop zone is added

--- master_thesis_user_interface/src/Components/js/AddNewDataset.js
+++ master_thesis_user_interface/src/Components/js/AddNewDataset.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-
+import Dropzone from 'react-dropzone'
 class AddNewDataset extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      files: []
+    }
+    this.onDrop = this.onDrop.bind(this)
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    })
+  }
   render() {
     return (
-      <div className="AddNewDataset">
-        My AddNewDataset
-      </div>
+      <section>
+         <div className="dropzone">
+           <Dropzone onDrop={this.onDrop.bind(this)}>
+             <p>Try dropping some files here, or click to select files to upload.</p>
+           </Dropzone>
+         </div>
+         <aside>
+           <h2>Dropped files</h2>
+           <ul>
+             {
+               this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+             }
+           </ul>
+         </aside>
+       </section>
     );
   }
 }
