1329b6d41b94ee9363f337fd24c5f0c2d625e854
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2018-01-08 21:02:25 +0100

    modify file to adopt dropdown from local folder using ajax & cors fixing

--- master_thesis_user_interface/src/Components/js/DataSetSelection.js
+++ master_thesis_user_interface/src/Components/js/DataSetSelection.js
@@ -1,31 +1,58 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class DataSetSelection extends Component {
   constructor(props){
     super(props);
-    this.state = {value: "Finance"};
+    this.state = {
+      receiveValues: []
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    console.log(event.target.value);
   }
 
   handleSubmit(event){
     console.log(this.state.value)
     event.preventDefault();
   }
+
+  componentDidMount(){
+    this.fetchDataFromDirectory();
+  }
+
+  fetchDataFromDirectory(){
+    $.ajax({
+      url: 'http://localhost:8080/upload/readAllFiles',
+      dataType: 'json',
+      cache: 'false',
+      success: function(data){
+        let items = [];
+        for (let i =0; i<data.length; i++){
+          console.log(data[i].id);
+          items.push(<option key={data[i].id} value={data[i].fileName}>{data[i].fileName}</option>);
+        }
+        this.setState({receiveValues: items}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log('', status, err);
+      }
+    });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
           <label>Category</label> <br />
           <div>
             <select value={this.state.value} onChange={this.handleChange}>
-               <option value="Finance">Finance</option>
-               <option value="Environment">Environment</option>
-               <option value="Technology">Technology</option>
+              {this.state.receiveValues}
            </select>
          </div>
          <br />
